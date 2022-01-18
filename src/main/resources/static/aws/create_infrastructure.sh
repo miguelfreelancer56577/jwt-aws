bbd6277525979d43721475cf242d61fa704e7243
Miguel Torres <miguelfreelancer56577@gmail.com>
2022-01-17 19:18:15 -0600

    Added steps to create aws services.

--- src/main/resources/static/aws/create_infrastructure.sh
+++ src/main/resources/static/aws/create_infrastructure.sh
@@ -1,4 +1,76 @@
 #!/bin/bash
 
-echo "Listing s3 buckets."
-aws s3 ls
+#declaring variables
+clusterName=$AWS_ENV'-cluster'
+
+isReserved(){
+
+	#checking if the resource already exists
+	rs=$(aws docdb describe-db-clusters --query 'length(DBClusters[?DBClusterIdentifier==`'$clusterName'`].DBClusterIdentifier)')
+	if [ $rs != 0 ]; then
+		echo "The resource already exists."
+		return 0
+	else
+		echo "The resource doesnt exist."
+		return 1
+	fi
+}
+
+createDocDBCluster(){
+
+	aws docdb create-db-cluster \
+		    --db-cluster-identifier $clusterName \
+	    --engine docdb \
+	    --master-username $AWS_DOCDB_USER \
+	    --master-user-password $AWS_DOCDB_PASS \
+	    --port 27017 \
+		--vpc-security-group-ids sg-0dff1c0c116919b3f \
+		--tags Key=environment,Value=$AWS_ENV \
+		--preferred-maintenance-window Sun:20:30-Sun:21:00 \
+		--no-deletion-protection \
+		--source-region $AWS_REGION \
+		--output text \
+		--profile $AWS_PROFILE
+		
+}
+
+attachInstances(){
+
+	rs='None'
+	echo "Creating instances."	
+	while [ $rs != 'available' ]
+	do
+	   echo "Checking status."	
+	   sleep 10
+	   rs=$(aws docdb describe-db-clusters --query 'DBClusters[?DBClusterIdentifier==`dev-cluster`].Status | [0]' --output text)
+	   echo "Cluster status. $rs"
+	done
+	aws docdb create-db-instance \
+    --db-cluster-identifier $clusterName \
+    --db-instance-class db.t4g.medium \
+    --db-instance-identifier $clusterName'-instance' \
+    --tags Key=environment,Value=$AWS_ENV \
+    --output text \
+    --engine docdb
+}
+
+main(){
+
+	#checking if the resource already exists
+	isReserved
+	if [ $? == 1 ]; then
+		echo "Creting documentdb cluster"
+		createDocDBCluster
+		if [ $? == 0 ]; then
+			echo "Documentdb cluster created successfully."
+			attachInstances
+			if [ $? == 0 ]; then
+			echo "Instance attached properly to the cluster."
+		fi
+		fi
+		
+	fi
+}
+
+#calling main function
+main
