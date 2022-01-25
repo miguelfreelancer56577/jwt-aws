b77e9f5a74aff1b811540dcc1950aed9fb038055
Miguel Torres <miguelfreelancer56577@gmail.com>
2022-01-25 11:45:46 -0600

    Added mongo dependencies.

--- src/main/java/com/github/mangelt/lab/Application.java
+++ src/main/java/com/github/mangelt/lab/Application.java
@@ -2,12 +2,14 @@ package com.github.mangelt.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-public class Lab11Application {
+@EnableMongoRepositories
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab11Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
